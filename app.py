164cee63452339daeb786688452db5a5f6ae01d9
ludanekrasova <79700631+ludanekrasova@users.noreply.github.com>
2023-08-25 23:11:52 +0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,24 @@
+#!flask/bin/python
+# -*- coding: utf-8 -*-
+from flask import Flask, render_template, request, session, render_template_string
+import pandas as pd
+import os
+
+app = Flask(__name__, static_url_path='/static')
+app.secret_key = "super secret key"
+
+@app.after_request
+def after_request(response):
+   response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate, public, max-age=0"
+   response.headers["Expires"] = 0
+   response.headers["Pragma"] = "no-cache"
+   return response
+
+def root_dir():  # pragma: no cover
+    return os.path.abspath(os.path.dirname(__file__))
+root = root_dir() + '/'
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
