932ace56446e8c80da7b89834932fa7995552e5d
ludanekrasova <79700631+ludanekrasova@users.noreply.github.com>
2023-08-26 11:24:09 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
-from flask import Flask
+from flask import Flask, request, render_template, session
+from utils import predict_type
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
+app.secret_key = "secret secret key"
 
-@app.route('/')
-def hello():
-    return "Hello, World!"
+menu = [{"name": "Поиск адреса", "url": ""},
+        {"name": "Результат", "url": "main"}]
+
+@app.route('/', methods=['GET', 'POST'])
+def main():
+    if request.method == 'GET':
+        return render_template('main.html', title="Проверка адреса", menu=menu)
+
+    if request.method == 'POST':
+        text = request.form['text']
+        session['text'] = text
+
+        prediction = predict_type(text)
+
+        return render_template('main.html', result=prediction,
+                               menu=menu)
 
 if __name__ == '__main__':
     app.run()
