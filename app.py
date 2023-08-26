be4d4ced3c523e1d9bc148cf27b53e8f432b61be
ludanekrasova <ludanekrasova@gmail.com>
2023-08-26 11:28:38 +0500

    Initial commit

--- app.py
+++ app.py
@@ -1,11 +1,26 @@
 from flask import Flask, request, render_template, session
-from utils import predict_type
+import pandas as pd
 
 app = Flask(__name__, template_folder='templates')
 app.secret_key = "secret secret key"
 
 menu = [{"name": "Поиск адреса", "url": ""},
         {"name": "Результат", "url": "main"}]
+
+data = pd.read_csv("data/building_20230808.csv")
+
+
+def predict_type(text):
+    '''Предсказание типа письма и типа заявки'''
+    data = pd.DataFrame(data={'text': [text]}, index=[0])
+
+    #prediction = model.predict(data)
+    #prediction = {"success": true, "query:": ["address": text], "result": [{"target_building_id": 209676 ,"target_address":  "г.Санкт-Петербург, Аптекарский проспект, дом 18, литера А" }]}
+    prediction = {"success": "true", "query:": ["address", text], "result": [{"target_building_id", 209676 ,"target_address",  "г.Санкт-Петербург, Аптекарский проспект, дом 18, литера А" }]}
+
+    return prediction
+
+
 
 @app.route('/', methods=['GET', 'POST'])
 def main():
